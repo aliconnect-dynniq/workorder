79990c23f3066ec9b7701a464362614287a4dc6e
Aliconnect <max@alicon.nl>
2020-09-03 14:12:43 +0200

    1.0.0

---
+++ js/config.js
@@ -0,0 +1,52 @@
+(function() {
+  AIM.extend({
+    components: {
+      schemas: {
+        Workorder: {
+          properties: {
+            // Configuratie Stap 1, Start opdracht naar opdrachtgever
+            Opdrachtgever: {
+              prompt: 'Start',
+              label: 'Basis gegevens',
+              description: 'Voer alle basis gegevens in voor het starten van de opdracht',
+              title: 'Geef naam opdrachtgever',
+            },
+            Opdrachtgever_bezoekadres: {
+              format: 'address',
+            },
+            Opdrachtgever_contactpersoon: {
+              format: 'text',
+            },
+            Opdrachtgever_email: {
+              format: 'email',
+              mailto: true,
+              required: true,
+              pattern: '[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$',
+              title: 'Geef email adres van opdrachtgever',
+            },
+            // Configuratie Stap 2
+            Uitvoerder: {
+              prompt: 'Stap 2',
+              label: 'Basis gegevens uitvoerder',
+              description: 'Voer alle basis gegevens van de uitvoerder in voor het starten van de opdracht',
+              title: 'Geef naam opdrachtgever',
+            },
+            Uitvoerder_bezoekadres: {
+              format: 'address',
+            },
+            Uitvoerder_contactpersoon: {
+              format: 'text',
+            },
+            Uitvoerder_email: {
+              format: 'email',
+              mailto: true,
+              required: true,
+              pattern: '[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$',
+              title: 'Geef email adres van uitvoerder',
+            },
+          }
+        }
+      }
+    },
+  });
+})();
--- js/workorder.js
+++ js/workorder.js
@@ -1,53 +1,5 @@
 (function() {
   AIM.extend({
-    components: {
-      schemas: {
-        Workorder: {
-          properties: {
-            // Configuratie Stap 1, Start opdracht naar opdrachtgever
-            Opdrachtgever: {
-              prompt: 'Start',
-              label: 'Basis gegevens',
-              description: 'Voer alle basis gegevens in voor het starten van de opdracht',
-              title: 'Geef naam opdrachtgever',
-            },
-            Opdrachtgever_bezoekadres: {
-              format: 'address',
-            },
-            Opdrachtgever_contactpersoon: {
-              format: 'text',
-            },
-            Opdrachtgever_email: {
-              format: 'email',
-              mailto: true,
-              required: true,
-              pattern: '[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$',
-              title: 'Geef email adres van opdrachtgever',
-            },
-            // Configuratie Stap 2
-            Uitvoerder: {
-              prompt: 'Stap 2',
-              label: 'Basis gegevens uitvoerder',
-              description: 'Voer alle basis gegevens van de uitvoerder in voor het starten van de opdracht',
-              title: 'Geef naam opdrachtgever',
-            },
-            Uitvoerder_bezoekadres: {
-              format: 'address',
-            },
-            Uitvoerder_contactpersoon: {
-              format: 'text',
-            },
-            Uitvoerder_email: {
-              format: 'email',
-              mailto: true,
-              required: true,
-              pattern: '[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$',
-              title: 'Geef email adres van uitvoerder',
-            },
-          }
-        }
-      }
-    },
     prompt: {
       init() {
         return colpanel.createElement('FORM', 'col', {
@@ -72,6 +24,9 @@
       },
     },
     on: {
+      load() {
+
+      },
       init() {
         // console.log('aaa',document.cookies);
         if (!AIM.get.prompt) AIM.request('?prompt=init');
