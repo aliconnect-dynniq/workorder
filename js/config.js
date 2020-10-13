d8b9c63b72a452943746fdb702addceb3c46fa5b
maxvankampen <max.van.kampen@alicon.nl>
2020-10-13 11:07:28 +0200

    1.0.0

--- js/config.js
+++ js/config.js
@@ -4,21 +4,120 @@
       schemas: {
         Workorder: {
           properties: {
-            
-            // Configuratie Stap 1, Start opdracht naar opdrachtgever
-            Opdrachtgever: {
-              prompt: 'Start',
-              label: 'Basis gegevens',
-              description: 'Voer alle basis gegevens in voor het starten van de opdracht',
-              title: 'Geef naam opdrachtgever',
+
+
+            State: {
+              options: {
+                New: { },
+                Requested: { },
+                Accepted: { },
+              }
             },
+
+            CreateDateTime: { },
+            LastModifiedDateTime: { },
+            LastModifiedBy: { },
+
+            FinishDateTime: { },
+
+            // Categorie: { },
+
+
+            // Configuratie Stap 1, Start opdracht naar opdrachtgever
+            Naam_uitvoerder: {
+
+              prompt: 'New',
+
+
+
+
+              description: 'Voer alle basis gegevens in voor het starten van de opdracht',
+
+              label: 'Basis_gegevens',
+
+              title: 'Geef naam uitvoerder',
+
+              // schema: 'Contact',
+              // filter: 'Type=uitvoerder',
+            },
+            StartDateTime: {
+            },
+            EndDateTime: {
+            },
+            Station: {
+              // schema: 'Station',
+
+              schema: 'System',
+              filter: 'Typical=Station',
+
+              options: {
+                J2: { },
+                C: { },
+                H: { },
+              }
+            },
+            Benodigde_uitschakeling: {
+
+              schema: 'System',
+              filter: 'Typical eq Energiegroep,TempRegelaar and Master eq @Station',
+
+              type: 'array',
+              // max: 3,
+            },
+            Werkzaamheden_uitvoerder: {
+              // format: 'textarea',
+              // format: 'div',
+              format: 'rich',
+            },
+            Requested_email: {
+              title: 'Geef email adres van opdrachtgever',
+              type: 'string',
+              format: 'email',
+              // pattern: '[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$',
+              mailto: true,
+              required: true,
+
+              defaultvalue: 'ams@schiphol.nl',
+
+              value: 'ams@schiphol.nl',
+              hidden: true,
+            },
+
+
+
+
+
+
+
+
+            xxxxx: {
+              prompt: 'Requested',
+            },
+
+
+
+
+
+
+
+
+
+
+
+
             Opdrachtgever_bezoekadres: {
+
+              prompt: 'Stap 2',
+
+
+
               format: 'address',
             },
             Opdrachtgever_contactpersoon: {
               format: 'text',
             },
             Opdrachtgever_email: {
+              type: 'string',
               format: 'email',
               mailto: true,
               required: true,
@@ -28,8 +127,9 @@
 
             // Configuratie Stap 2
             Uitvoerder: {
-              prompt: 'Stap 2',
-              label: 'Basis gegevens uitvoerder',
+
+
+              label: __('Basis gegevens uitvoerder'),
               description: 'Voer alle basis gegevens van de uitvoerder in voor het starten van de opdracht',
               title: 'Geef naam opdrachtgever',
             },
@@ -38,6 +138,16 @@
             },
             Uitvoerder_contactpersoon: {
               format: 'text',
+            },
+            Uitvoerder_signature: {
+              type: 'image',
+              format: 'signature',
+            },
+            Uitvoerder_locatie: {
+              format: 'location',
+            },
+            Uitvoerder_image_lock: {
+              format: 'cam',
             },
             Uitvoerder_email: {
               format: 'email',
--- js/workorder.js
+++ js/workorder.js
@@ -1,84 +1,10 @@
 (function() {
-  AIM.extend({
-    prompt: {
-      init() {
-        if (AIM.cookie.id_token) return AIM.prompt('welcome');
-        // console.warn('welkom_quest');
-        return colpanel.createElement('FORM', 'col', {
-          title:'Welkom',
-          description: 'Om u aan te melden klik op login of scan onderstaande QR code met uq Aliconnect login app',
-          hyperlinks: [
-            ['SPAN', 'login_qr_code', { qr: {
-              text: AIM.config.aim.websocket.socket_id ? 'https://login.aliconnect.nl?s=' + AIM.config.aim.websocket.socket_id : '',
-      				width: 160,
-      				height: 160,
-            } }],
-          ],
-          operations: {
-            login: {type: 'button', onclick() { AIM.login({scope: AIM.auth.scope }); } },
-          },
-          // onshow() {
-          //   AIM.login();
-          // },
-          onsubmit(event) {
-            event.preventDefault();
-            let submitter = event.submitter || event.target.submitter;
-            AIM.request('?prompt=' + submitter.value);
-          },
-        });
-      },
-      welcome() {
-        console.log('WELCOME');
-        return colpanel.createElement('FORM', 'col', {
-          title: 'Aanmaken opdracht',
-          description: `Welkom ${AIM.auth.name}, Wilt u een opdracht aanmaken? klik dan op Volgende.`,
-          properties: {
-
-          },
-          // hyperlinks: [
-          //   { label: 'Afmelden', href: '#?prompt=logout' },
-          // ],
-          operations: {
-            next: {type: 'submit' },
-            logout: {type: 'button', onclick: AIM.logout },
-          },
-          client: AIM.config.aim,
-          action: `/Workorder`,
-          onload(event) {
-            console.log(event.body);
-            AIM.get.id = event.body.UID;
-            AIM.get.id = event.body.ID;
-            AIM.request('?prompt=Start&id=' + AIM.get.id);
-            // AIM.request('?prompt=' + event.submitter.value);
-          },
-        });
-      },
-    },
-    on: {
-      logout() {
-        document.location.href = '/';
-      },
-      init() {
-        console.log('Workorder.js.init', document.cookie);
-        // if (!AIM.auth.id_token) {
-        //   AIM.login({prompt: 'accept', scope: 'name+email'});
-        // }
-        if (!AIM.get.prompt) {
-          AIM.request('?prompt=init');
-        }
-        // AIM.prompt('Stap 1');
-      }
-    }
-  });
   let step = {};
-  // let label = 'Start';
   let config = null;
   Panel = function (config) {
-    // console.log(config.label, config);
     return () => {
-      // console.log(config.label);
+      console.log('CLIENT', AIM.config.aim);
       return colpanel.createElement('FORM', 'col', {
-
         // label: config.label ,
         // pos:'m',
         client: AIM.config.aim,
@@ -103,22 +29,94 @@
       });
     }
   }
-  for (let [propertyName, property] of Object.entries(AIM.components.schemas.Workorder.properties)) {
-    if (property.prompt) {
-      if (config) {
-        config.next = property.prompt;
+  // Omzetten van AIM.components.schemas.Workorder.properties naar losse panels voor het werkproces
+  AIM.extend({
+    auth: { scope: 'name+email', },
+    prompt: {
+      init() {
+        if (AIM.cookie.id_token) return AIM.prompt('welcome');
+        // console.warn('welkom_quest');
+        return colpanel.createElement('FORM', 'col', {
+          title:'Welkom',
+          description: 'Om u aan te melden klik op login of scan onderstaande QR code met uq Aliconnect login app',
+          hyperlinks: [
+            ['SPAN', 'login_qr_code', { qr: {
+              text: AIM.config.aim.websocket.socket_id ? 'https://login.aliconnect.nl?s=' + AIM.config.aim.websocket.socket_id : '',
+      				width: 160,
+      				height: 160,
+            } }],
+            ['A', '', 'Homepage', {href: '/'}],
+          ],
+          operations: {
+            login: {type: 'button', onclick() { AIM.auth.login({scope: AIM.auth.scope }); } },
+          },
+          // onshow() {
+          //   AIM.auth.login();
+          // },
+          onsubmit(event) {
+            event.preventDefault();
+            let submitter = event.submitter || event.target.submitter;
+            AIM.request('?prompt=' + submitter.value);
+          },
+        });
+      },
+      welcome() {
+        console.log('WELCOME');
+        return colpanel.createElement('FORM', 'col', {
+          title: 'Aanmaken opdracht',
+          description: `Welkom ${AIM.auth.name}, Wilt u een opdracht aanmaken? klik dan op Volgende.`,
+          properties: {
+
+          },
+          // hyperlinks: [
+          //   { label: 'Afmelden', href: '#?prompt=logout' },
+          // ],
+          operations: {
+            next: {type: 'submit' },
+            logout: {type: 'button', onclick: AIM.auth.logout },
+          },
+          client: AIM.config.aim,
+          action: `/Workorder`,
+          onload(event) {
+            console.log(event.body);
+            AIM.get.id = event.body.UID;
+            AIM.get.id = event.body.ID;
+            AIM.request('?prompt=Start&id=' + AIM.get.id);
+            // AIM.request('?prompt=' + event.submitter.value);
+          },
+        });
+      },
+    },
+    on: {
+      logout() {
+        // document.location.href = 'https://login.aliconnect.nl?prompt=logout&redirect_uri=' + document.location.origin;
+        document.location.href = 'https://login.aliconnect.nl?prompt=logout&redirect_uri=' + document.location.origin + document.location.pathname;
+      },
+      load() {
+        for (let [propertyName, property] of Object.entries(AIM.components.schemas.Workorder.properties)) {
+          if (property.prompt) {
+            if (config) {
+              config.next = property.prompt;
+            }
+            config = Object.assign({
+              // label: property.label,
+              prior: config ? config.prompt : null,
+              properties: {},
+              operations: {},
+            }, property);
+            AIM.prompt[config.prompt] = new Panel(config);
+          }
+          // console.log(propertyName, config);
+          config.properties[propertyName] = property;
+          if (property.mailto) config.mailto = propertyName;
+        }
+      },
+      init() {
+        if (!AIM.get.prompt) {
+          AIM.request('?prompt=init');
+        }
       }
-      config = Object.assign({
-        // label: property.label,
-        prior: config ? config.prompt : null,
-        properties: {},
-        operations: {},
-      }, property);
-      AIM.prompt[config.prompt] = new Panel(config);
     }
-    // console.log(propertyName, config);
-    config.properties[propertyName] = property;
-    if (property.mailto) config.mailto = propertyName;
-  }
+  });
   // console.log(AIM.prompt.Step_1, AIM.prompt.Step_2);
 })();
